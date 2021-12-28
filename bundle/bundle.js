995775c869e0a5fef0aca325bcf00480eb1e8d11
Ian McGahren <fullstackeidolon@gmail.com>
2021-12-28 16:32:19 -0500

    Updated CSS and assets files

--- bundle/bundle.js
+++ bundle/bundle.js
@@ -43654,7 +43654,6 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
 /* harmony import */ var _assets_QASearchButtonIcon_png__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(50);
-/* harmony import */ var _assets_QASearchButtonIcon_png__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_assets_QASearchButtonIcon_png__WEBPACK_IMPORTED_MODULE_1__);
 function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -43707,7 +43706,7 @@ var QuestionsAndAnswersHeader = /*#__PURE__*/function (_React$Component) {
         className: "quesSearchButton",
         type: "submit"
       }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("img", {
-        source: __webpack_require__(50)
+        source: _assets_QASearchButtonIcon_png__WEBPACK_IMPORTED_MODULE_1__["default"]
       }))));
     }
   }]);
@@ -43719,9 +43718,14 @@ var QuestionsAndAnswersHeader = /*#__PURE__*/function (_React$Component) {
 
 /***/ }),
 /* 50 */
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-throw new Error("Module parse failed: Unexpected character '�' (1:0)\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\n(Source code omitted for this binary file)");
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + "c6c509b55945e48f95b32ff4108c8a7b.png");
 
 /***/ }),
 /* 51 */
@@ -43966,6 +43970,18 @@ var RatingsAndReviews = /*#__PURE__*/function (_React$Component) {
 /******/ 		};
 /******/ 	})();
 /******/ 	
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
 /******/ 	(() => {
 /******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
@@ -43980,6 +43996,26 @@ var RatingsAndReviews = /*#__PURE__*/function (_React$Component) {
 /******/ 			}
 /******/ 			Object.defineProperty(exports, '__esModule', { value: true });
 /******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) scriptUrl = scripts[scripts.length - 1].src
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
 /******/ 	})();
 /******/ 	
 /************************************************************************/
--- client/QuestionsAndAnswers/QuestionsAndAnswersHeader.jsx
+++ client/QuestionsAndAnswers/QuestionsAndAnswersHeader.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import QASearchButtonIcon from '../assets/QASearchButtonIcon.png'
 
-
 class QuestionsAndAnswersHeader extends React.Component {
   constructor (props) {
     super(props)
@@ -15,7 +14,7 @@ class QuestionsAndAnswersHeader extends React.Component {
         <form className="quesSearchForm">
           <input type='text' className='quesSearchField'/>
           <button className='quesSearchButton' type='submit'>
-            <img source={require('../assets/QASearchButtonIcon.png')} />
+            <img source={QASearchButtonIcon} />
           </button>
         </form>
       </div>
