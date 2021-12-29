13d12f227e6ce8ec996bc36553e754397adc2a6c
Ian McGahren <fullstackeidolon@gmail.com>
2021-12-29 15:09:54 -0500

    Cleaned up some console logs

--- client/App.jsx
+++ client/App.jsx
@@ -44,7 +44,7 @@ class App extends React.Component {
     axios
       .get(`/reviews?product_id=${this.state.product_id}&count=2&sort=relevant`)
       .then(function (response) {
-        console.log('response Data:', response.data.results)
+        //console.log('response Data:', response.data.results)
         that.setState({
           reviews: response.data.results
         })
