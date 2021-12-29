cf38b496c2273c8bc7d9112223d8a8ac4cc3262b
Winston Pantelakos <winstonspantelakos@gmail.com>
2021-12-29 13:34:16 -0500

    minor changes made

--- client/App.jsx
+++ client/App.jsx
@@ -1,127 +1,110 @@
 /* Author: */
 //Import Library Dependencies
-import React from 'react'
-import $ from 'jquery'
-import axios from 'axios'
-import ReactDOM from 'react-dom'
+import React from "react";
+import $ from "jquery";
+import axios from "axios";
+import ReactDOM from "react-dom";
 
 //Import Modules
-import { ProductDetails } from './ProductDetails/ProductDetails.jsx'
-import { RelatedProducts } from './RelatedProducts/RelatedProducts.jsx'
-import { QuestionsAndAnswers } from './QuestionsAndAnswers/QuestionsAndAnswersMainWrapper.jsx'
-import { RatingsAndReviews } from './RatingsAndReviews/RatingsAndReviews.jsx'
-import { ProductGallery } from './ProductDetails/ProductGallery.jsx'
+import { ProductDetails } from "./ProductDetails/ProductDetails.jsx";
+import { RelatedProducts } from "./RelatedProducts/RelatedProducts.jsx";
+import { QuestionsAndAnswers } from "./QuestionsAndAnswers/QuestionsAndAnswersMainWrapper.jsx";
+import { RatingsAndReviews } from "./RatingsAndReviews/RatingsAndReviews.jsx";
+import { ProductGallery } from "./ProductDetails/ProductGallery.jsx";
 
 class App extends React.Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      product_id: 38204, //DUMMY VALUE, gets passed to all components
+      product_id: 38204,
       reviews: [],
       reviewCount: 2,
       ratings: {},
-<<<<<<< HEAD
-      recommendedPercentage: 0
-
+      recommendedPercentage: 0,
     };
     this.onMoreReviewsClick = this.onMoreReviewsClick.bind(this);
     this.getReviews = this.getReviews.bind(this);
-=======
-      recommendedPercentage: 0,
-      ratingsCount: 0
-    }
->>>>>>> 5a3ee7ef430042f036d3aa556567a3c13fa612ff
   }
-  getMetadata () {
-    let that = this
+  getMetadata() {
+    let that = this;
 
     axios
       .get(`/reviews/meta?product_id=${this.state.product_id}`)
       .then(function (response) {
         that.setState({
           ratings: response.data.ratings,
-<<<<<<< HEAD
-          recommendedPercentage: response.data.recommended
-        });
-=======
           recommendedPercentage: response.data.recommended,
-          ratingsCount: response.data.ratingsCount
-        })
->>>>>>> 5a3ee7ef430042f036d3aa556567a3c13fa612ff
+        });
       })
       .catch(function (error) {
-        console.log('Metadata GET Error:', error)
-      })
+        console.log("Metadata GET Error:", error);
+      });
   }
-  getReviews () {
-    let that = this
+  getReviews() {
+    let that = this;
     axios
-      .get(`/reviews?product_id=${this.state.product_id}&count=${this.state.reviewCount}&sort=relevant`)
+      .get(
+        `/reviews?product_id=${this.state.product_id}&count=${this.state.reviewCount}&sort=relevant`
+      )
       .then(function (response) {
-        console.log('response Data:', response.data.results)
+        console.log("response Data:", response.data.results);
         that.setState({
-          reviews: response.data.results
-        })
+          reviews: response.data.results,
+        });
       })
       .catch(function (error) {
-        console.log('Reviews GET Error:', error)
-      })
+        console.log("Reviews GET Error:", error);
+      });
   }
-<<<<<<< HEAD
   onMoreReviewsClick(callback) {
     let that = this;
 
     let newCount = this.state.reviewCount + 2;
 
     let addReviews = () => {
-      return new Promise (function (resolve, reject) {
-        that.setState({reviewCount: newCount}, function (error, result) {
+      return new Promise(function (resolve, reject) {
+        that.setState({ reviewCount: newCount }, function (error, result) {
           if (error) {
-            reject (error);
+            reject(error);
           } else {
             resolve(result);
           }
-        })
-      })
-    }
+        });
+      });
+    };
     addReviews()
-      .then(function(result) {
+      .then(function (result) {
         that.getReviews();
       })
-      .catch(function(error) {
+      .catch(function (error) {
         console.log("More Reviews Error:", error);
-      })
+      });
   }
   componentDidMount() {
     this.getMetadata();
     this.getReviews();
-=======
-  componentDidMount () {
-    this.getMetadata()
-    this.getReviews()
->>>>>>> 5a3ee7ef430042f036d3aa556567a3c13fa612ff
   }
-
-  render () {
+  render() {
     return (
-      <div className='App'>
-        <div className='container'>
-          <div className='header'>header</div>
-          <div className='banner'>banner</div>
-          <div className='wrapper'>
-            <ProductDetails product_id={this.state.product_id} />
-            <RelatedProducts product_id={this.state.product_id} />
-            <QuestionsAndAnswers product_id={this.state.product_id} />
+      <div className="App">
+        <div className="container">
+          <div className="header">header</div>
+          <div className="banner">banner</div>
+          <div className="wrapper">
+            <ProductDetails />
+            <RelatedProducts />
+            <QuestionsAndAnswers />
             <RatingsAndReviews
               ratings={this.state.ratings}
               recommended={this.state.recommendedPercentage}
-              reviews={this.state.reviews} onClick={this.onMoreReviewsClick}
+              reviews={this.state.reviews}
+              onClick={this.onMoreReviewsClick}
             />
           </div>
-          <div className='footer'>footer</div>
+          <div className="footer">footer</div>
         </div>
       </div>
-    )
+    );
   }
 }
-export { App }
+export { App };
