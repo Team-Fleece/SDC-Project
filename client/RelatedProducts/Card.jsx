8af3dca3d560bb64c1d506fcbd29a341a3fc75f7
Kacy Holm <[email>
2021-12-28 17:10:26 -0700

    Built basic related products skeleton

---
+++ client/RelatedProducts/Card.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+//image img
+//information
+//actions
+
+export default function Card() {
+  return <div> helo this is card </div>
+}
--- client/RelatedProducts/RelatedProducts.jsx
+++ client/RelatedProducts/RelatedProducts.jsx
@@ -1,24 +1,36 @@
-/* Author: */
+/* Author: Kacy*/
 //Import Library Dependencies
 import React from 'react'
+import Card from './Card.jsx'
+
 
 class RelatedProducts extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.state = {
 
     }
   }
 
-render() {
-  return (
-    <div className='rItemsCompare'>
-  rItemsCompare
-  <div className='relatedProducts'>relatedProducts</div>
-  <div className='favoriteProducts'>favoriteProducts</div>
-</div>
+  render() {
+    return (
+      <div className='rItemsCompare'>
+        rItemsCompare
+        <div className='relatedProducts'>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        <div className='favoriteProducts'>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
 
-  )
-}
+    )
+  }
 }
 export { RelatedProducts }
