fff19fd9b37e2c5f2b0216098ab9dc2b83a5a764
Ian McGahren <60856620+FullStackEidolon@users.noreply.github.com>
2021-12-30 14:44:09 -0500

    Revert "Haydenn2"

--- client/App.jsx
+++ client/App.jsx
@@ -16,7 +16,7 @@ class App extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      product_id: 37316,
+      product_id: 38204, //DUMMY VALUE, gets passed to all components
     }
     this.onRelatedProductClick = this.onRelatedProductClick.bind(this)
   }
@@ -26,10 +26,8 @@ class App extends React.Component {
   componentDidMount() {
     this.onRelatedProductClick
   }
-
-  render(){
-    return(
-
+  render () {
+    return (
       <div id='App' className='App'>
         <div className='container'>
           <div className='header'>header</div>
