dd2dd1e8987f1de7aeebad0682adcecd664bb7f3
Haydenn Harper <haydennharper@gmail.com>
2022-01-03 12:16:53 -0500

    About to pull

--- client/App.jsx
+++ client/App.jsx
@@ -62,7 +62,23 @@ class App extends React.Component {
     return (
       <div id='App' className='App'>
         <div className='container'>
-          <div className='header'> <img src="https://see.fontimg.com/api/renderfont4/3z8d8/eyJyIjoiZnMiLCJoIjo0NywidyI6MTAwMCwiZnMiOjQ3LCJmZ2MiOiIjRkFGOEY4IiwiYmdjIjoiIzBFMDAwMCIsInQiOjF9/Q2x1dGNo/rooster-personal-use.png" alt="Cursive fonts"></img> </div>
+          <div className='header'> <img src="https://see.fontimg.com/api/renderfont4/3z8d8/eyJyIjoiZnMiLCJoIjo0NywidyI6MTAwMCwiZnMiOjQ3LCJmZ2MiOiIjRkFGOEY4IiwiYmdjIjoiIzBFMDAwMCIsInQiOjF9/Q2x1dGNo/rooster-personal-use.png" alt="Cursive fonts"></img>
+
+
+          <div className='QASearchForm'>
+          <button className='QASearchButton' type='submit'>
+          {/* <FontAwesomeIcon icon="fa-regular fa-magnifying-glass" /> */}
+            {/* <img src={QASearchButtonIcon} /> */}
+          </button>
+          <input type='text' placeholder='Search Questions' className='QASearchField' />
+
+          <button className='QASearchClear' type='reset'>
+            {/* <img src={QASearchClearIcon} /> */}
+
+          </button>
+        </div>
+
+          </div>
           <div className='banner'>
           <marquee style={{ color: 'red', fontSize: '1em' }}>  <strong>Members only sale</strong><i> - Sign up now to receive exclusive deals!</i>  </marquee>
             </div>
